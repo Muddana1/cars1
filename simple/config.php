39f75d116a4cc1f576933abd10e74ffc35a191ae
Muddana1 <60711540+Muddana1@users.noreply.github.com>
2021-04-07 22:14:45 -0400

    Update config.php

--- simple/config.php
+++ simple/config.php
@@ -1,15 +1,12 @@
 <?php
 /* Database credentials. */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'muddanan1');
-define('DB_PASSWORD', 'dszu5s');
-define('DB_NAME', 'muddanan1_db');
- 
+define('DB_SERVER', 'us-cdbr-east-03.cleardb.com');
+define('DB_USERNAME', 'b0ede556e62e4b');
+define('DB_PASSWORD', '170d8cad');
+define('DB_NAME', 'heroku_fd6d25c0e48f745');
+define('DB_USERNAME', 'beb59f30ab670f');
+define('DB_PASSWORD', 'fd9ba7ef');
+define('DB_NAME', 'heroku_2d004d87e93034c');
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-   die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>

45292fdcd9863447d104a9c215efbf9bea74e63c
Muddana1 <60711540+Muddana1@users.noreply.github.com>
2021-04-07 22:29:05 -0400

    Update config.php

--- simple/config.php
+++ simple/config.php
@@ -1,9 +1,6 @@
 <?php
 /* Database credentials. */
 define('DB_SERVER', 'us-cdbr-east-03.cleardb.com');
-define('DB_USERNAME', 'b0ede556e62e4b');
-define('DB_PASSWORD', '170d8cad');
-define('DB_NAME', 'heroku_fd6d25c0e48f745');
 define('DB_USERNAME', 'beb59f30ab670f');
 define('DB_PASSWORD', 'fd9ba7ef');
 define('DB_NAME', 'heroku_2d004d87e93034c');

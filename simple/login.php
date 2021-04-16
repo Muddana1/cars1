61c674c13835f92e2faa74e9bddce35ce64fa6c5
Muddana1 <60711540+Muddana1@users.noreply.github.com>
2021-04-16 13:43:05 -0400

    Update login.php

--- simple/login.php
+++ simple/login.php
@@ -31,7 +31,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                mysqli_stmt_store_result($stmt);
                
                // Check if username exists, if yes then verify password
-               if(mysqli_stmt_num_rows($stmt) == 0){                    
+               if(mysqli_stmt_num_rows($stmt) == 1){                    
                    // Bind result variables
                    mysqli_stmt_bind_result($stmt, $carname, $h_carmodel);
 			if(mysqli_stmt_fetch($stmt)){
